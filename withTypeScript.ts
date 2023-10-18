2f748f03f0afd9753361e262a79e5584b22aee5d
CTC CCP 6 <113914772+8i-ght@users.noreply.github.com>
2023-10-18 13:16:33 -0500

    finish typescript

--- withTypeScript.ts
+++ withTypeScript.ts
@@ -1,31 +1,51 @@
 const startButton = document.querySelector('[data-action="start"]') as HTMLInputElement;
 const stopButton = document.querySelector('[data-action="stop"]') as HTMLInputElement;
 const resetButton = document.querySelector('[data-action="reset"]') as HTMLInputElement;
-const minutes = document.querySelector('.minutes');
-const seconds = document.querySelector('.seconds') as HTMLInputElement;
+const minutes = document.querySelector('.minutes') as HTMLInputElement | HTMLElement;
+const seconds = document.querySelector('.seconds') as HTMLInputElement | HTMLElement;
 let timerTime: number = 0;
-
-setInterval(function() {
-    timerTime++;
-
-    const numOfMinutes = Math.floor(timerTime / 60);
-    const numOfSeconds = timerTime % 60;
-
-    minutes.innerText = numOfMinutes;
-    seconds.innerText = timerTime;
-}, 1000);
-
+let isRunning: boolean = false;
+let interval: any;
 
 function startTimer() {
+    if (isRunning) return;
 
+    isRunning = true;
+    interval = setInterval(incrementTimer, 1000);
 }
 
 function stopTimer() {
+    if (!isRunning) return;
 
+    isRunning = false;
+    clearInterval(interval);
 }
 
-function resetTimer() {
+function resetTimer(): void {
+    stopTimer();
 
+    timerTime = 0;
+    minutes.innerText = '00';
+    seconds.innerText = '00';
+}
+
+function incrementTimer() {
+        timerTime++;
+    
+        const numOfMinutes = Math.floor(timerTime / 60);
+        const numOfSeconds = timerTime % 60;
+    
+        minutes.innerText = pad(numOfMinutes) as any;
+        seconds.innerText = pad(numOfSeconds) as any;
+}
+
+function pad(num: number) {
+    return (num < 10) ? '0' + num : num;
+    // if (num < 10) {
+    //     return '0' + num;
+    // } else {
+    //     return num;
+    // }
 }
 
 startButton.addEventListener('click', startTimer);
